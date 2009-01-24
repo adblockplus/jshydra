3de5bab644910af88ad0d855cd0def0ecdcbccea
Joshua Cranmer <Pidgeot18@gmail.com>
2009-01-24 16:54:16 -0500

    Added a test directory.
    Per request of taras, to have a file that I know works.

---
+++ tests/basic.js
@@ -0,0 +1,9 @@
+// Run ../jshydra basic.js basic.js: it should work.
+// This is a simple test just to make sure that something isn't borking with
+// jshydra.
+function process_js(ast) {
+	for (f in ast) {
+		_print(f + ": "+ ast[f]);
+	}
+	_print(uneval(ast));
+}
