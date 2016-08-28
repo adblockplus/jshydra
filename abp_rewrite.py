62beff6abbdd7edb109e85b6424af5befcaa0d1b
Sebastian Noack <sebastian@adblockplus.org>
2016-08-28 23:50:47 +0200

    Issue 4374 - Made Python code comply with our coding practices and added flake8

--- abp_rewrite.py
+++ abp_rewrite.py
@@ -3,13 +3,81 @@
 # http://mozilla.org/MPL/2.0/.
 
 import os
+import platform
+import io
+import zipfile
 import subprocess
 
-import utils
+try:
+    from urllib.request import urlopen
+except ImportError:
+    import urllib2
+    import contextlib
+
+    def urlopen(*args, **kwargs):
+        return contextlib.closing(urllib2.urlopen(*args, **kwargs))
+
+JSSHELL_DIR = 'mozilla-esr31'
+JSSHELL_URL = ('https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly'
+               '/2015/02/2015-02-25-00-22-19-{}'
+               '/jsshell-{{}}.zip'.format(JSSHELL_DIR))
+
+JSSHELL_SUPPORTED_PLATFORMS = {
+    'Windows': 'win32',
+    'Linux': {
+        'i686': 'linux-i686',
+        'x86_64': 'linux-x86_64'
+    },
+    'Darwin': 'mac'
+}
+
+
+def ensure_jsshell():
+    path = os.environ.get('SPIDERMONKEY_BINARY')
+    if path and os.path.isfile(path):
+        return path
+
+    system = platform.system()
+    try:
+        build = JSSHELL_SUPPORTED_PLATFORMS[system]
+        if isinstance(build, dict):
+            build = build[platform.machine()]
+    except KeyError:
+        raise Exception('Platform {} ({}) not supported by JS shell'.format(
+            system, platform.machine()
+        ))
+
+    shell_dir = os.path.join(os.path.dirname(__file__),
+                             '{}-{}'.format(JSSHELL_DIR, build))
+    if not os.path.exists(shell_dir):
+        os.makedirs(shell_dir)
+    if system == 'Windows':
+        path = os.path.join(shell_dir, 'js.exe')
+    else:
+        path = os.path.join(shell_dir, 'js')
+
+    if os.path.exists(path):
+        return path
+
+    with urlopen(JSSHELL_URL.format(build)) as response:
+        data = response.read()
+
+    with zipfile.ZipFile(io.BytesIO(data)) as archive:
+        archive.extractall(shell_dir)
+
+    if not os.path.exists(path):
+        raise Exception("Downloaded package doesn't contain JS shell")
+
+    try:
+        os.chmod(path, 0o700)
+    except:
+        pass
+
+    return path
 
 
 def rewrite_js(args, script=None):
-    jsshell = utils.ensureJSShell()
+    jsshell = ensure_jsshell()
     env = {'LD_LIBRARY_PATH': os.path.relpath(os.path.dirname(jsshell))}
     base_dir = os.path.dirname(__file__)
 
--- autotest.py
+++ autotest.py
@@ -13,30 +13,34 @@ import abp_rewrite
 
 
 def run_tests():
-    testDir = os.path.join(os.path.dirname(__file__), 'autotest')
+    test_dir = os.path.join(os.path.dirname(__file__), 'autotest')
     succeed = True
 
-    for file in os.listdir(testDir):
-        if not re.search(r'^test_.*\.js$', file):
+    for filename in os.listdir(test_dir):
+        if not re.search(r'^test_.*\.js$', filename):
             continue
 
-        file = os.path.join(testDir, file)
-        handle = open(file, 'r')
+        filename = os.path.join(test_dir, filename)
         name = None
-        arguments = None
-        for line in handle:
-            match = re.search(r'^//\s*([A-Za-z]+):\s*(.*?)\s*$', line)
-            if match and match.group(1).lower() == 'name':
-                name = match.group(2)
-            elif match and match.group(1).lower() == 'arguments':
-                arguments = match.group(2).split(' ')
-        handle.close()
+        args = None
 
-        if arguments == None:
+        with open(filename, 'r') as file:
+            for line in file:
+                match = re.search(r'^//\s*([A-Za-z]+):\s*(.*?)\s*$', line)
+                if match:
+                    key = match.group(1).lower()
+                    if key == 'name':
+                        name = match.group(2)
+                    elif key == 'arguments':
+                        args = match.group(2).split()
+
+        if args is None:
             continue
 
-        output = abp_rewrite.rewrite_js(arguments, file)
-        expected = open(file + '.expected', 'rU').read()
+        output = abp_rewrite.rewrite_js(args, filename)
+        with open(filename + '.expected', 'rU') as file:
+            expected = file.read()
+
         if output == expected:
             print(name + ' passed')
         else:
@@ -44,8 +48,8 @@ def run_tests():
             print(name + ' failed! Log:')
             for line in difflib.unified_diff(expected.splitlines(),
                                              output.splitlines(),
-                                             fromfile=file + '.expected',
-                                             tofile=file + '.output'):
+                                             fromfile=filename + '.expected',
+                                             tofile=filename + '.output'):
                 print(line)
             print()
 
